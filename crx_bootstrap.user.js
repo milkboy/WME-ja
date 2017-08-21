d4bc7c2858ecbf31fd343acd8fe560e74a5b3b8b
Michael Wikberg <waze@wikberg.fi>
2017-08-21 12:11:10 +0300

    Add packaging for chrome extension

--- crx_bootstrap.user.js
+++ crx_bootstrap.user.js
@@ -4,7 +4,7 @@
 // @description         Show the angle between two selected (and connected) segments
 // @include             /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor.*$/
 // @updateURL           https://github.com/milkboy/WME-ja/raw/master/wme_junctionangle.user.js
-// @version             1.12.999
+// @version             ${project.version}
 // @grant               GM_getResourceURL
 // @copyright           2016 Michael Wikberg <michael@wikberg.fi>
 // @run_at              document_start
